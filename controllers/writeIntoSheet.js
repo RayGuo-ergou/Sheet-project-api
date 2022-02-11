9e134746cb27c12f1f283746e50560c5e19190a2
RayGuo-ergou <ma2808203259@hotmail.com>
2022-02-12 04:48:01 +1100

    added date checker

--- controllers/writeIntoSheet.js
+++ controllers/writeIntoSheet.js
@@ -26,7 +26,9 @@ const writeIntoSheet = async function (client, req, res, next) {
     dataArray.push(checkNullValue(data.BookingName));
     dataArray.push('');
     dataArray.push(checkNullValue(data.NumberofPeople));
-    dataArray.push(checkNullValue(data.TimeofArrival));
+    dataArray.push(
+      checkDate(data.Date) + ', ' + checkNullValue(data.TimeofArrival)
+    );
     dataArray.push(checkNullValue(data.ContactNumber));
     dataArray.push(checkNullValue(data.Referral));
     dataArray.push(checkNullValue(data.Manager));
@@ -74,6 +76,122 @@ const checkNullValue = (data) => {
     //return data
     return data;
   }
+};
+
+const checkDate = (date) => {
+  // check if date has chinese
+  if (hasChinese(date)) {
+    // if it has chinese, convert it to number
+    return reverseDate(convertChineseDate(date));
+  } else {
+    // check if date has english
+    if (hasEnglish(date)) {
+      // get english characters
+      const english = getEnglish(date);
+      // convert month from english to number
+      const month = getMonthFromString(english);
+
+      // if month is -1, return date
+      if (month === -1) {
+        return 'error! please input manually';
+      } else {
+        // get all numbers from date
+        const day = date.match(/\d+/g).join('');
+        // combine month and day
+        return `${day}/${month}`;
+      }
+    } else {
+      // if date is null or undefined, space return ''
+      if (!date || date === '') {
+        return '';
+      }
+      return date;
+    }
+  }
+};
+
+// check if any chinese character in a string
+const hasChinese = (str) => {
+  return /[\u4e00-\u9fa5]/.test(str);
+};
+
+//reverse month and day eg: 11/1 =>> 1/11
+const reverseDate = (str) => {
+  const date = str.split('/');
+  // if date[0] length is 3 remove char[1]
+  if (date[0].length === 3) {
+    // remove char[1]
+    date[0] = date[0].slice(0, 1) + date[0].slice(2);
+  }
+
+  // if data[1] length is 4 remove the second and third char
+  if (date[1].length === 4) {
+    // remove char[1]
+    date[1] = date[1].slice(0, 1) + date[1].slice(3);
+  }
+
+  // if data[1] length is 3 remove the second char
+  if (date[1].length === 3) {
+    // remove char[1]
+    date[1] = date[1].slice(0, 1) + date[1].slice(2);
+  }
+
+  return `${date[1]}/${date[0]}`;
+};
+
+const convertChineseDate = (str) => {
+  return str.replace(
+    /年|月|日|一|二|三|四|五|六|七|八|九|十/g,
+    function (match) {
+      switch (match) {
+        case '月':
+          return '/';
+        case '日':
+          return '';
+        case '一':
+          return '1';
+        case '二':
+          return '2';
+        case '三':
+          return '3';
+        case '四':
+          return '4';
+        case '五':
+          return '5';
+        case '六':
+          return '6';
+        case '七':
+          return '7';
+        case '八':
+          return '8';
+        case '九':
+          return '9';
+        case '十':
+          return '10';
+        default:
+          return match;
+      }
+    }
+  );
+};
+
+// convert month from english to number
+function getMonthFromString(mon) {
+  var d = Date.parse(mon + ' 1, 2021');
+  if (!isNaN(d)) {
+    return new Date(d).getMonth() + 1;
+  }
+  return -1;
+}
+
+// check if any english character in a string
+const hasEnglish = (str) => {
+  return /[a-zA-Z]/.test(str);
+};
+
+//only get english characters in a string, make it string
+const getEnglish = (str) => {
+  return str.match(/[a-zA-Z]/g).join('');
 };
 
 // export the writeIntoSheet method
--- test.js
+++ test.js
@@ -50,10 +50,120 @@
 // console.log(onlySpaces('')); // 👉️ true
 
 //remove all spaces in a string
-const removeSpaces = (str) => {
-  return str.replace(/\s/g, '');
+// const removeSpaces = (str) => {
+//   return str.replace(/\s/g, '');
+// };
+// console.log(removeSpaces('   ')); // 👉️ ''
+// console.log(removeSpaces('  123  ')); // 👉️ '123'
+// console.log(removeSpaces('hello ')); // 👉️ 'hello'
+// console.log(removeSpaces('')); // 👉️ ''
+
+//reverse month and day eg: 11/1 =>> 1/11
+const reverseDate = (str) => {
+  const date = str.split('/');
+  // if date[0] length is 3 remove char[1]
+  if (date[0].length === 3) {
+    // remove char[1]
+    date[0] = date[0].slice(0, 1) + date[0].slice(2);
+  }
+
+  // if data[1] length is 4 remove the second and third char
+  if (date[1].length === 4) {
+    // remove char[1]
+    date[1] = date[1].slice(0, 1) + date[1].slice(3);
+  }
+
+  return `${date[1]}/${date[0]}`;
 };
-console.log(removeSpaces('   ')); // 👉️ ''
-console.log(removeSpaces('  123  ')); // 👉️ '123'
-console.log(removeSpaces('hello ')); // 👉️ 'hello'
-console.log(removeSpaces('')); // 👉️ ''
+
+const convertChineseDate = (str) => {
+  return str.replace(
+    /年|月|日|一|二|三|四|五|六|七|八|九|十/g,
+    function (match) {
+      switch (match) {
+        case '月':
+          return '/';
+        case '日':
+          return '';
+        case '一':
+          return '1';
+        case '二':
+          return '2';
+        case '三':
+          return '3';
+        case '四':
+          return '4';
+        case '五':
+          return '5';
+        case '六':
+          return '6';
+        case '七':
+          return '7';
+        case '八':
+          return '8';
+        case '九':
+          return '9';
+        case '十':
+          return '10';
+        default:
+          return match;
+      }
+    }
+  );
+};
+// if the date has chinese characters, convert it to english
+var str = '二月四日';
+console.log(reverseDate(convertChineseDate(str)));
+
+function getMonthFromString(mon) {
+  var d = Date.parse(mon + ' 1, 2020');
+  console.log(mon + ' 1, 2020');
+  console.log(`d`);
+  console.log(d);
+  if (!isNaN(d)) {
+    return new Date(d).getMonth() + 1;
+  }
+  return -1;
+}
+
+console.log(getMonthFromString('FebRuary'));
+
+// check if any chinese character in a string
+const hasChinese = (str) => {
+  return /[\u4e00-\u9fa5]/.test(str);
+};
+
+// check if any english character in a string
+const hasEnglish = (str) => {
+  return /[a-zA-Z]/.test(str);
+};
+
+//only get english characters in a string, make it string
+const getEnglish = (str) => {
+  return str.match(/[a-zA-Z]/g).join('');
+};
+
+console.log(getEnglish('feb/01'));
+
+// // check if any month word in a string like january
+// const hasMonth = (str) => {
+//   // convert str to lowercase
+//   const lowerStr = str.toLowerCase();
+
+//   return /january|february|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|april/.test(
+//     lowerStr
+//   );
+// };
+
+// console.log(hasMonth('jan')); // 👉️ true
+// console.log(hasMonth('FeBruary')); // 👉️ true
+// console.log(hasMonth('march')); // 👉️ true
+// console.log(hasMonth('april')); // 👉️ true
+// console.log(hasMonth('may')); // 👉️ true
+// console.log(hasMonth('june')); // 👉️ true
+// console.log(hasMonth('july')); // 👉️ true
+// console.log(hasMonth('august')); // 👉️ true
+// console.log(hasMonth('september')); // 👉️ true
+// console.log(hasMonth('october')); // 👉️ true
+// console.log(hasMonth('november')); // 👉️ true
+// console.log(hasMonth('december')); // 👉️ true
